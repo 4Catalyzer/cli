a82c7fd4404600156a6185cb4f6b71e049343667
Jason Quense <monastic.panic@gmail.com>
2019-01-15 14:16:16 -0500

    feat: typescript scaffolding support

--- packages/init/plopfile.js
+++ packages/init/plopfile.js
@@ -57,11 +57,17 @@ const prompts = [
     message: 'What type of library is this?',
   },
   {
+    name: 'typescript',
+    type: 'confirm',
+    default: false,
+    message: 'Do want top use TypeScript?',
+  },
+  {
     name: 'babel',
     type: 'confirm',
     default: false,
     message: 'Do you need babel (maybe not?)',
-    when: _ => _.type === 'node',
+    when: _ => _.type === 'node' && !_.typescript,
   },
   {
     name: 'semanticRelease',
@@ -82,10 +88,10 @@ module.exports = plop => {
     description: 'create a new package',
     prompts,
     actions(answers) {
-      const { type, location } = answers;
+      const { type, location, typescript } = answers;
       const workspaceRoot = getRoot(location);
       // mutate so templates have the correct value
-      if (type === 'web') answers.babel = true; // eslint-disable-line no-param-reassign
+      if (type === 'web' || typescript) answers.babel = true; // eslint-disable-line no-param-reassign
       const data = {
         workspaceRoot,
         includeEslint: !workspaceRoot && type === 'web',
@@ -148,21 +154,29 @@ module.exports = plop => {
         },
         () => GitUtilities.init(location).catch(ignore),
         _ => GitUtilities.addRemote(location, _.name).catch(ignore),
-        answers.babel
-          ? {
-              type: 'add',
-              path: `{{location}}/.babelrc.js`,
-              templateFile: `${templatePath}/babelrc.js.hbs`,
-              skipIfExists: true,
-              data,
-            }
-          : {
-              type: 'add',
-              path: `{{location}}/index.js`,
-              templateFile: `${templatePath}/index.js.hbs`,
-              skipIfExists: true,
-              data,
-            },
+        answers.typescript && {
+          type: 'add',
+          path: `{{location}}/tsconfig.json`,
+          templateFile: `${templatePath}/tsconfig.json`,
+          skipIfExists: true,
+          data,
+        },
+        answers.babel && {
+          type: 'add',
+          path: `{{location}}/.babelrc.js`,
+          templateFile: `${templatePath}/babelrc.js.hbs`,
+          skipIfExists: true,
+          data,
+        },
+        {
+          type: 'add',
+          path: `{{location}}/${answers.babel ? 'src/' : ''}index.${
+            answers.typescript ? 'ts' : 'js'
+          }`,
+          templateFile: `${templatePath}/index.js.hbs`,
+          skipIfExists: true,
+          data,
+        },
         () =>
           execa(
             'prettier',
